b095385896286b5eaeaf3ef626df6628f5d2532d
yemyokyaw <yemyokyaw.etp@gmail.com>
2024-07-31 10:10:39 +0630

    project set up

---
+++ src/main/java/com/example/inbound_backend/InboundBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.inbound_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InboundBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InboundBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inbound_backend/InboundBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inbound_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InboundBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
